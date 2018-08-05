5935109ff77a422290a18382dd78742a331d30d0
Christopher Forkner <cforkner@hitachims.local>
2018-08-05 11:18:50 -0600

    Working with Tables

--- SAP/Get-SAPProcessList.psm1
+++ SAP/Get-SAPProcessList.psm1
@@ -1,9 +1,9 @@
-function Get-SAP {
+function Get-SAPProcessList {
   [cmdletbinding()]
-  Param([String] $Host, [Int] $NR,[Parameter(ValueFromPipeline=$true)]$pipe)
+  Param([String] $Name, [Int] $NR,[Parameter(ValueFromPipeline=$true)]$pipe)
   
 
-Begin {
+  Begin {
   $SOAPRequest = @"                                                                           
 <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:SAPControl="urn:SAPControl" xmlns:SAPCCMS="urn:SAPCCMS" xmlns:SAPHostControl="urn:SAPHostControl" xmlns:SAPLandscapeService="urn:SAPLandscapeService" xmlns:SAPMetricService="urn:SAPMetricService" xmlns:SAPOscol="urn:SAPOscol" xmlns:SAPDSR="urn:SAPDSR">
 <SOAP-ENV:Body><SAPControl:GetProcessList></SAPControl:GetProcessList>
@@ -13,11 +13,11 @@ Begin {
 
 
   function GetProcessList($instance){
-    $URL = 'http://'+$Host+':5'+$instance+'13?wsdl'
+    $URL = 'http://'+$Name+':5'+$instance+'13?wsdl'
     $proxy = Invoke-WebRequest -Uri $URL -Method Post -Body $SOAPRequest -ContentType "text/xml"
     $testxml = [XML]($proxy.RawContent.Split([Environment]::NewLine) | Select-String -Pattern "<SOAP-ENV:Envelope xmlns")
-    $xml2 = [XML]($testxml.Envelope.Body.GetProcessListResponse.process.OuterXml)
-    $gpl = $xml2.process.ChildNodes | Format-Table
+    $xml2 = [XML]($testxml.Envelope.Body.GetProcessListResponse.process.OuterXml)  
+    $gpl = $xml2.process.item | Select-Object @{Label="NR"; Expression={$instance}}, @{Label="Description"; Expression={$_.description}}, @{Label="Status"; Expression={$_.dispstatus -replace 'SAPControl-',''}}, @{Label="Text"; Expression={$_.textstatus}}, @{Label="Uptime"; Expression={$_.elapsedtime}}, @{Label="Started";Expression={$_.starttime}}, @{Label="Host"; Expression={$Name}}
     return $gpl
 
   } 
@@ -36,14 +36,31 @@ Begin {
 }
 Process{
   if ($pipe){
-    $Host = $_
+    if ($_.Name){ 
+      $Name = $_.Name 
+    } else {
+      $name = $_
+    }
   }
-  $Host.ToUpper()
+  #$Name.ToUpper()
     try {
-      GetProcessList($as) 
-    } catch { }
-    
-      GetProcessList($disp)
+      $ast = GetProcessList($as) 
+    } catch { 
+      
+    }
+    try {
+      $dst = GetProcessList($disp)
+    } catch {
+      Write-Output "Nothing found on $Name $disp"
+    }
+
+    if ($ast){
+      $final = $ast + $dst
+    } else {
+      $final = $dst
+    }
+
+    $final | Select-Object Host,NR,Description,Status,Uptime
   }
 End {}
   
