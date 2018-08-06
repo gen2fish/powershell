d8fb7f2dfdd6a1bc68a04a48d203610599a82306
Christopher Forkner <cforkner@hitachims.local>
2018-08-05 20:04:46 -0600

    Documentation!

--- SAP/Get-SAPInstance.psm1
+++ SAP/Get-SAPInstance.psm1
@@ -1,4 +1,48 @@
 function Get-SAPInstance {
+    <#
+    .SYNOPSIS
+    Gets the Instances for a Remote SAP System.
+    
+    .DESCRIPTION
+    Uses SAPControl WSDL to get the Instance List of a Remote SAP System. 
+    Can accept a pipe input of Host or Name
+
+    .EXAMPLE
+    PS C:\Users\Administrator> Get-SAPInstance hhd-tys4hx01
+    
+    Host         NR Description            Status                                   
+    ----         -- -----------            ------                                   
+    hhd-tys4hx01 00 ABAP|GATEWAY|ICMAN|IGS GREEN                                    
+    hhd-tys4hx01 01 MESSAGESERVER|ENQUE    GREEN
+
+    Gets SAP System at Instance number of 00
+
+    .EXAMPLE
+    PS C:\Users\Administrator> Get-SAPInstance hhd-tys4hd01 -NR 01
+
+    Host         NR Description            Status                                   
+    ----         -- -----------            ------                                   
+    HHD-TYS4HD01 02 MESSAGESERVER|ENQUE    GREEN                                    
+    HHD-TYS4HD01 01 ABAP|GATEWAY|ICMAN|IGS GREEN
+
+    Gets SAP System at Instance number of 01
+    
+    .EXAMPLE
+    PS C:\Users\Administrator> Get-VM -Name HHD-TYWEBD01 | Get-SAPInstance
+
+    Host         NR Description Status                                              
+    ----         -- ----------- ------                                              
+    hhd-tywebd01 00 WEBDISP     GREEN
+
+    Gets SAP System at Instance number of 00 of the found VM 
+
+    .NOTES
+    Author: Christopher D Forkner
+    Date:  5 August 2018
+
+    Must be able to connect to the sapsystem
+
+    #>
   [cmdletbinding()]
   Param([String] $Name, [Int] $NR,[Parameter(ValueFromPipeline=$true)]$pipe)
   
@@ -62,6 +106,7 @@ function Get-SAPInstance {
   End {}
 }
 function _GetSOAP($function){
+  #This formats the SOAP Request to SAPControl. Not meant to be called directly.
   $Request = @"                                                                           
 <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:SAPControl="urn:SAPControl" xmlns:SAPCCMS="urn:SAPCCMS" xmlns:SAPHostControl="urn:SAPHostControl" xmlns:SAPLandscapeService="urn:SAPLandscapeService" xmlns:SAPMetricService="urn:SAPMetricService" xmlns:SAPOscol="urn:SAPOscol" xmlns:SAPDSR="urn:SAPDSR">
 <SOAP-ENV:Body><SAPControl:$function></SAPControl:$function>
@@ -72,6 +117,87 @@ function _GetSOAP($function){
 }
   
 function Get-SAPProcessList {
+    <#
+    .SYNOPSIS
+    Gets the Process List for a Remote SAP System.
+    
+    .DESCRIPTION
+    Uses SAPControl WSDL to get the Process List of a Remote SAP System.
+    Can accept a pipe from Get-SAPInstance.
+
+    .EXAMPLE
+    PS C:\Users\Administrator> Get-SAPProcessList hhd-tys4hx01 -nr 00
+
+    Host        : hhd-tys4hx01
+    Description : Dispatcher
+    Status      : GREEN
+    Uptime      : 105:53:13
+
+    Host        : hhd-tys4hx01
+    NR          : 00
+    Description : IGS Watchdog
+    Status      : GREEN
+    Uptime      : 105:53:13
+
+    Host        : hhd-tys4hx01
+    NR          : 00
+    Description : Gateway
+    Status      : GREEN
+    Uptime      : 105:52:42
+
+    Host        : hhd-tys4hx01
+    NR          : 00
+    Description : ICM
+    Status      : GREEN
+    Uptime      : 105:52:42
+
+    Gets the Process List of the SAP System at Instance number 00
+
+    .EXAMPLE
+    PS C:\Users\Administrator> Get-SAPInstance hhd-tys4hx01 | Get-SAPProcessList
+
+    Host        : hhd-tys4hx01                                                                                                                                                      NR          : 00                                                                                                                                                                
+    Description : Dispatcher
+    Status      : GREEN
+    Uptime      : 105:54:46
+
+    Host        : hhd-tys4hx01
+    NR          : 00
+    Description : IGS Watchdog
+    Status      : GREEN
+    Uptime      : 105:54:46
+
+    Host        : hhd-tys4hx01
+    NR          : 00
+    Description : Gateway
+    Status      : GREEN
+    Uptime      : 105:54:15
+
+    Host        : hhd-tys4hx01
+    NR          : 00
+    Description : ICM
+    Status      : GREEN
+    Uptime      : 105:54:15
+
+    Host        : hhd-tys4hx01                                                                                                                                                      NR          : 01                                                                                                                                                                Description : MessageServer                                                                                                                                                     Status      : GREEN                                                                                                                                                             
+    Uptime      : 105:54:59
+
+    Host        : hhd-tys4hx01
+    NR          : 01
+    Description : EnqueueServer
+    Status      : GREEN
+    Uptime      : 105:54:59
+
+    Gets the Instance List from hhd-tys4hx01 then all processes for all found SAP Systems
+    
+    .NOTES
+    Author: Christopher D Forkner
+    Date:  5 August 2018
+
+    Must be able to connect to the sapsystem
+
+    #>
+
   [cmdletbinding()]
   Param([String] $Name, [Int] $NR,[Parameter(ValueFromPipeline=$true)]$pipe)
   
@@ -110,7 +236,7 @@ function Get-SAPProcessList {
     }
 
     $disp = '0'+$NR.ToString()
-    $SOAPRequest = GetSOAP('GetProcessList')
+    $SOAPRequest = _GetSOAP('GetProcessList')
     try {
       $dst = GetProcessList($disp)
     } catch {
